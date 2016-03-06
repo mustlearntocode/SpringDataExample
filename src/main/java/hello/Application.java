63db5d5d218064be46a51b9d3e0e117bda575965
Xiuyi Ye <xiuyiye@gmail.com>
2016-03-06 14:23:13 -0500

    completed the application.java
    also, fixed the looger and loggerFactory library

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,16 +1,51 @@
 package hello;
 
-import org.apache.log4j.spi.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 
 public class Application {
 // logger is an singleton, is it thread-safe declared as instant variable like this??
 	
-//	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class);
+	}
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository){
+		return (args) -> {
+			//save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brain"));
+			repository.save(new Customer("Kim", "Baucer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+			
+			//fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for(Customer customer : repository.findAll()){
+				log.info(customer.toString());
+			}
+			log.info("");
+			
+			//fetch an individual customer by ID
+			Customer customer = repository.findOne(1L);
+			log.info("Customer found with findOne(1L)");
+			log.info("-------------------------------");
+			log.info(customer.toString());
+			log.info("");
+			
+			//fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'");
+			log.info("------------------------------------------");
+			for(Customer bauer : repository.findByLastName("Beauer")){
+				log.info(bauer.toString());
+			}
+			log.info("");
+		};
 	}
 	
 
