5d217c6ed43ac2e482eba73f1eaa2cc4b971ee5a
Xiuyi Ye <xiuyiye@gmail.com>
2016-03-06 14:44:23 -0500

    added @SpringBootApplication

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,8 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+@SpringBootApplication
 public class Application {
 // logger is an singleton, is it thread-safe declared as instant variable like this??
 	
